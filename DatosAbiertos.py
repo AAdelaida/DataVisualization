9f993884c539e7849f2abc147442d7cc5f5f44d4
Araceli Adelaida Carreño Ibáñez <52715150+AAdelaida@users.noreply.github.com>
2023-02-25 07:59:50 -0600

    Update DatosAbiertos.py

--- DatosAbiertos.py
+++ DatosAbiertos.py
@@ -1,18 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import streamlit as st
 import pandas as pd
 import base64
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-
-
-# In[ ]:
 
 
 # Dar formato a los datos
