531ca1005a15d60e893146ac43208f92f0b37023
Araceli Adelaida Carreño Ibáñez <52715150+AAdelaida@users.noreply.github.com>
2023-02-25 08:39:54 -0600

    Add files via upload

--- DatosAbiertos_app.py
+++ DatosAbiertos_app.py
@@ -190,7 +190,7 @@ if not df_contribuyentesActivos.empty:
     st.pyplot(fig)
 
 
-st.markdown("""Movimientos al RFC por año""")
+st.markdown("""Movimientos al RFC por año (información disponible a partir de 2015""")
 
 # Generar gráfica de movimientos al RFC
 if not df_inscripciones_seleccionados.empty:
